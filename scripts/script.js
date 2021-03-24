110e3e8c0bf53b5046518bf4effe78eb90aaef67
Ramitax <ramiro.ambrosetti@hotmail.com>
2021-03-24 04:47:19 -0300

    Base project

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+menu = document.getElementById('menu')
+header = document.querySelector('header')
+navbar = document.querySelectorAll('.navbar__item')
+form = document.querySelector('form')
+data = document.querySelectorAll('input.box, textarea.box')
+btn = document.querySelector('.submit')
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times')
+    header.classList.toggle('toggle')
+})
+navbar.forEach(btn => {
+    btn.addEventListener('click', () => {
+        menu.classList.remove('fa-times')
+        header.classList.remove('toggle')
+    })
+})
+
+form.addEventListener('submit', (e) => {
+    /* 
+  e.preventDefault()
+    */
+})
+
+btn.addEventListener('click', (e)=>{
+    data.forEach(register => {
+        console.log(register.value)
+        register.value = ""
+    })
+})
