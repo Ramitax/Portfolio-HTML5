a1a1732b06939761ca27b0b38d87f60fef786ecd
Ramitax <ramiro.ambrosetti@hotmail.com>
2021-03-26 20:16:28 -0300

    Correcion de texto

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ navbar = document.querySelectorAll('.navbar__item')
 form = document.querySelector('form')
 data = document.querySelectorAll('input.box, textarea.box')
 btn = document.querySelector('.submit')
+covid = document.getElementById('covid')
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times')
@@ -17,14 +18,16 @@ navbar.forEach(btn => {
 })
 
 form.addEventListener('submit', (e) => {
-    /* 
-  e.preventDefault()
-    */
+    e.preventDefault()
 })
 
-btn.addEventListener('click', (e)=>{
+btn.addEventListener('click', (e) => {
     data.forEach(register => {
         console.log(register.value)
         register.value = ""
     })
+})
+
+covid.addEventListener('click', (e) => {
+    window.location.href("https://github.com/Ramitax?tab=repositories")
 })
